ef7fed2863b97363796d49302aa230942fc3331d
Antti Virtanen <antti.ok.virtanen@gmail.com>
2016-08-29 00:05:42 +0300

    update to get_course_data and done first data fetch

--- get_course_data.py
+++ get_course_data.py
@@ -6,27 +6,68 @@ f_out = open('course_data.csv', 'w', encoding='utf-8')
 
 for line in f_courses:
     print('Course: ' + line)
-    url = 'https://mycourses.aalto.fi/course/info.php?lang=en&id=' + line.rstrip()
+    page_url = 'https://mycourses.aalto.fi/course/info.php?lang=en&id=' + line.rstrip()
     print(page_url)
 
     local_filename, headers = urllib.request.urlretrieve(page_url)
     html = open(local_filename, encoding='utf-8')
     content = html.read()
-    position = 0
 
-    f_out.write(line + ';')
+    f_out.write(line.rstrip() + ';')
 
-    search = 'Schedule:</b>'
+    search = 'summary_left'
+    start = content.find(search)
+    search = '<p'
+    start = content.find(search, start)
+    search = '>'
+    start = content.find(search, start)
+
+    if(start != -1):
+        start += len(search)
+        end = content.find('<', start) - 1
+        split = content[start:end].split(' ')
+        f_out.write(split[0] + ';')
+        start += len(split[0]) + 1
+        f_out.write(content[start:end] + ';')
+    else:
+        f_out.write(';')
+
+    search = '<p'
+    start = content.find(search, start)
+    search = '>'
+    start = content.find(search, start)
+
+    if(start != -1):
+        start += len(search) + len(split[0]) + 1
+        end = content.find('<', start) - 1
+        f_out.write(content[start:end] + ';')
+    else:
+        f_out.write(';')
+
+    search = '<p'
+    start = content.find(search, start)
+    search = '>'
+    start = content.find(search, start)
+
+    if(start != -1):
+        start += len(search) + len(split[0]) + 1
+        end = content.find('<', start) - 1
+        f_out.write(content[start:end] + ';')
+    else:
+        f_out.write(';')
+
+
+
+    search = 'Schedule:</b> '
     start = content.find(search)
     if(start != -1):
         start += len(search)
         end = content.find('<', start) - 1
         f_out.write(content[start:end] + ';')
-        position = end
     else:
         f_out.write(';')
 
-    search = 'Teaching Period:</b>'
+    search = 'Teaching Period:</b> '
     start = content.find(search)
     if(start != -1):
         start += len(search)
@@ -35,12 +76,12 @@ for line in f_courses:
     else:
         f_out.write(';')
 
-    search = 'Credits:</b>'
+    search = 'Credits:</b> '
     start = content.find(search)
     if(start != -1):
         start += len(search)
         end = content.find('<', start) - 1
-        f_out.write(content[start:end] + '\n')
+        f_out.write(content[start:end])
 
     f_out.write('\n')
 
