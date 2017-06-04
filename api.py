c985f70615e4abc2a3cc0e9927dcdadc16b998fa
Antti Virtanen <you@example.com>
2017-06-04 17:20:09 +0300

    fixed typo

--- api.py
+++ api.py
@@ -98,7 +98,7 @@ def login():
     if (request.forms.get('username') and request.forms.get('password')):
         password_hash = hashlib.sha224(str.encode(request.forms.get('password'))).hexdigest()
         user = db.query(User).filter(and_(User.username.like(request.forms.get('username')), User.password_hash.like(password_hash)).first()
-        if(len(user == 1)):
+        if(len(user) == 1):
             return {'token':'123'}
     return {'token':''}
 
