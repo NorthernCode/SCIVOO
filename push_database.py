dcb71fb48da25ae17d41b79bcf3606b7738bd65e
Antti Virtanen <you@example.com>
2017-07-09 15:39:44 +0300

    fixed function order

--- push_database.py
+++ push_database.py
@@ -3,6 +3,40 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import re
 
+
+def parseRomans(periods):
+    result = periods.replace('III', '3')
+    result = result.replace('II', '2')
+    result = result.replace('IV', '4')
+    result = result.replace('I', '1')
+    result = result.replace('V', '5')
+    return result
+    
+def parsePeriod(periods):
+    result = ''
+    periods = periods.replace('¿', '-')
+    for char in periods:
+        if(char == 'I' or char == 'V' or char == ',' or char == '-'):
+            result += char
+        elif(char == ' ' ):
+            continue
+        else:
+            return parseRomans(result.strip())
+    return parseRomans(result.strip())
+
+def getStartingPeriods(periods):
+    result = ''
+    starts = True
+    for char in periods:
+        if(char == ','):
+            starts = True
+            result += char
+        elif(char == '-'):
+            starts = False
+        elif(starts):
+            result += char
+    return result
+
 engine = create_engine('sqlite:///scivoo_sqlalchemy.db')
 Base.metadata.bind = engine
 
@@ -34,37 +68,3 @@ if(len(admin) == 0):
 	db.add(item)
 
 db.commit()
-
-
-def parsePeriod(periods):
-    result = ''
-    periods = periods.replace('¿', '-')
-    for char in periods:
-        if(char == 'I' or char == 'V' or char == ',' or char == '-'):
-            result += char
-        elif(char == ' ' ):
-            continue
-        else:
-            return parseRomans(result.strip())
-    return parseRomans(result.strip())
-
-def parseRomans(periods):
-    result = periods.replace('III', '3')
-    result = result.replace('II', '2')
-    result = result.replace('IV', '4')
-    result = result.replace('I', '1')
-    result = result.replace('V', '5')
-    return result
-
-def getStartingPeriods(periods):
-    result = ''
-    starts = True
-    for char in periods:
-        if(char == ','):
-            starts = True
-            result += char
-        elif(char == '-'):
-            starts = False
-        elif(starts):
-            result += char
-    return result
