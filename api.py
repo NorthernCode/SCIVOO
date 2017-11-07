4bf9089206c616e4b82422ec8e1cabbdf68fdc97
Antti Virtanen <you@example.com>
2017-11-07 15:21:08 +0200

    show wrong hash

--- api.py
+++ api.py
@@ -135,7 +135,7 @@ def login():
             user.expires = math.floor(time.time()) + 600
             db.commit()
             return {'token': token}
-    return {'token':'', 'message':'login failed'}
+    return {'token':'', 'message':'login failed with hash', 'hash': password_hash}
 
 @post('/isadmin')
 def check_admin():
